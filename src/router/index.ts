33a9e88f5d51684c23a7022db8e1c962b275a141
小沈曰 <409178030@qq.com>
2022-03-29 22:50:04 +0800

    home

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,7 +9,18 @@ const routes: Array<RouteConfig> = [
   { path: '/login', component: () => import('@/views/Login/Login.vue') },
   // 404 页面的路由规则
   { path: '/404', component: () => import('@/views/404.vue') },
-  { path: '/', component: () => import('@/views/Main/Main.vue') },
+  {
+    path: '/',
+    component: () => import('@/views/Main/Main.vue'),
+    redirect: '/home',
+    children: [
+      // 子路由
+      // 注意：子路由的 path 有两种写法：
+      // 1. 完整写法：父 + 子
+      // 2. 简化写法：子，同时子路由的 path 不能以 / 开头
+      { path: 'home', component: () => import('@/views/Home/Home.vue') }
+    ]
+  },
   // 添加 * 所表示的通配符路由规则，表示如果输入的路径，在上面匹配不到要展示的组件，
   // 则自动重定向到 /404 这个路由地址
   { path: '*', redirect: '/404' }
